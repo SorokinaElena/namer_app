bc7990acad620aa76ca54acbd0689b3546c3f6ba
eleno <e.lenoksls@gmail.com>
2024-02-27 08:51:37 +0100

    responsiveness for nav_menu added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+// Flutter provides several widgets that help you make your apps automatically responsive. For example, Wrap is a widget similar to Row or Column that automatically wraps children to the next "line" (called "run") when there isn't enough vertical or horizontal space. There's FittedBox, a widget that automatically fits its child into available space according to your specifications.
+// The widget to use, in this case, is LayoutBuilder. It lets you change your widget tree depending on how much available space you have
+// But NavigationRail doesn't automatically show labels when there's enough space because it can't know what is enough space in every context. It's up to you, the developer, to make that call.
+// Say you decide to show labels only if MyHomePage is at least 600 pixels wide.
+// Note: Flutter works with logical pixels as a unit of length. They are also sometimes called device-independent pixels. A padding of 8 pixels is visually the same regardless of whether the app is running on an old low-res phone or a newer ‘retina' device. There are roughly 38 logical pixels per centimeter, or about 96 logical pixels per inch, of the physical display.
+
+// use Flutter's Refactor menu in VS Code to make the required changes:
+// Inside _MyHomePageState's build method, put your cursor on Scaffold.
+// Call up the Refactor menu.
+// Select Wrap with Builder and press Enter.
+// Modify the name of the newly added Builder to LayoutBuilder.
+// Modify the callback parameter list from (context) to (context, constraints).
+
+// *
+// LayoutBuilder's builder callback is called every time the constraints change. This happens when, for example:
+// The user resizes the app's window
+// The user rotates their phone from portrait mode to landscape mode, or back
+// Some widget next to MyHomePage grows in size, making MyHomePage's constraints smaller
+
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -44,40 +64,71 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {                                    
-    
-    return Scaffold(                                                     
-      body: Row(                                                                 //  new MyHomePage contains a Row with two children
-        children: [
-          SafeArea(                                                              // The SafeArea ensures that its child is not obscured by a hardware notch or a status bar. In this app, the widget wraps around NavigationRail to prevent the navigation buttons from being obscured by a mobile status bar, for example
-            child: NavigationRail(
-              extended: false,                                                   // change the extended false line in NavigationRail to true shows the labels next to the icons. This is possible to do automatically when the app has enough horizontal space
-              destinations: [
-                NavigationRailDestination(
-                icon: Icon(Icons.home),
-                label: Text('Home'),
+class MyHomePage extends StatefulWidget {                                         // convert MyHomePage to a stateful widget - Place cursor on the first line of MyHomePage and call up the Refactor menu, select Convert to StatefulWidget                                         
+  @override                                                                       // setState in _MyHomePageState intead of selectedIndex: 0 - The new stateful widget only needs to track one variable: selectedIndex
+  State<MyHomePage> createState() => _MyHomePageState();                          // The underscore (_) at the start of _MyHomePageState makes that class private and is enforced by the compiler
+}
+
+class _MyHomePageState extends State<MyHomePage> {  
+
+  var selectedIndex = 0;                    
+                                                                                  
+  @override                                                                       // This class extends State, and can therefore manage its own values. (It can change itself.)
+  Widget build(BuildContext context) {                                            // build method from the old, stateless widget has moved to the _MyHomePageState (instead of staying in the widget). It was moved verbatim—nothing inside the build method changed                                   
+
+    Widget page;                                                                  // declares a new variable, page, of the type Widget
+    switch (selectedIndex) {                                                      // a switch statement assigns a screen to page, according to the current value in selectedIndex
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();                                                     // Placeholder; a handy widget that draws a crossed rectangle wherever you place it, marking that part of the UI as unfinished
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');                 // the fail-fast principle - an error if selectedIndex is neither 0 or 1
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {                                              // *
+        return Scaffold(                                                     
+          body: Row(                                                                 //  new MyHomePage contains a Row with two children
+            children: [
+              SafeArea(                                                              // The SafeArea ensures that its child is not obscured by a hardware notch or a status bar. In this app, the widget wraps around NavigationRail to prevent the navigation buttons from being obscured by a mobile status bar, for example
+                child: NavigationRail(
+                  // extended: false,                                                   // change the extended false line in NavigationRail to true shows the labels next to the icons. This is possible to do automatically when the app has enough horizontal space
+                  // extended: true,                                                                   // make the navigation rail responsive. That is to say, make it automatically show the labels (using extended: true) when there's enough room for them
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                    ),
+                  ],
+                  // selectedIndex: 0,                                              //  A selected index of zero selects the first destination of the navigation rail, a selected index of one selects the second destination, and so on. For now, it's hard coded to zero.
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {                                  // The navigation rail also defines what happens when the user selects one of the destinations with onDestinationSelected. Right now, the app merely outputs the requested index value with print()
+                  // print('selected: $value');
+                  setState(() {                                                     // When the onDestinationSelected callback is called, instead of merely printing the new value to console, you assign it to selectedIndex inside a setState() call. This call is similar to the notifyListeners() method used previously—it makes sure that the UI updates
+                      selectedIndex = value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                icon: Icon(Icons.favorite),
-                label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: 0,                                                 //  A selected index of zero selects the first destination of the navigation rail, a selected index of one selects the second destination, and so on. For now, it's hard coded to zero.
-              onDestinationSelected: (value) {                                  // The navigation rail also defines what happens when the user selects one of the destinations with onDestinationSelected. Right now, the app merely outputs the requested index value with print()
-              print('selected: $value');
-              },
-            ),
+              ),
+              Expanded(                                                             // Expanded widgets are extremely useful in rows and columns—they let you express layouts where some children take only as much space as they need (SafeArea, in this case) and other widgets should take as much of the remaining room as possible (Expanded, in this case). Two Expanded widgets split all the available horizontal space between themselves, even though the navigation rail only really needed a little slice on the left.
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  // child: GeneratorPage(),
+                  child: page,                                                      // The app now switches between our GeneratorPage and the placeholder that will soon become the Favorites page
+                )
+              ),
+            ],
           ),
-          Expanded(                                                             // Expanded widgets are extremely useful in rows and columns—they let you express layouts where some children take only as much space as they need (SafeArea, in this case) and other widgets should take as much of the remaining room as possible (Expanded, in this case). Two Expanded widgets split all the available horizontal space between themselves, even though the navigation rail only really needed a little slice on the left.
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
-            )
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
